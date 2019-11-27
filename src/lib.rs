bb10ada48d2a48b33af8f2d1af5386f2db50239d
Yohaï-Eliel Berreby <yberreby@protonmail.ch>
2019-11-27 16:01:07 +0100

    fix test to get errors; allow 0 statements in list

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,5 @@ lalrpop_mod!(pub grammar); // synthesized by LALRPOP
 
 #[test]
 fn trivial_fn_decl() {
-    assert!(grammar::FuncParser::new().parse("fn hello() { (); }").is_ok());
+    grammar::FuncParser::new().parse("fn hello( ) { (); }").unwrap();
 }
