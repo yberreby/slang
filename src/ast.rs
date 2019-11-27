b89584ab3ef4e5bc3d38a9b95feffbe4544835e3
Yohaï-Eliel Berreby <yberreby@protonmail.ch>
2019-11-27 11:42:13 +0100

    add basic AST

---
+++ src/ast.rs
@@ -0,0 +1,38 @@
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum Expr {
+    Ref(Ident),
+    Arith(Op, Box<Expr>, Box<Expr>),
+    Unary(UnaryOp, Box<Expr>),
+    Num(i32),
+}
+
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum UnaryOp {
+    Not,
+}
+
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Eq,
+    Lte,
+    Gte,
+    And,
+    Or,
+    Lt,
+    Gt,
+    Ne,
+    BinOr,
+    BinAnd,
+    LShift,
+    RShift,
+}
+
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct Ident(pub String);
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+pub mod ast;
+
 #[macro_use] extern crate lalrpop_util;
 
 lalrpop_mod!(pub grammar); // synthesized by LALRPOP
@@ -8,4 +10,10 @@ fn calculator1() {
     assert!(grammar::TermParser::new().parse("(22)").is_ok());
     assert!(grammar::TermParser::new().parse("((((22))))").is_ok());
     assert!(grammar::TermParser::new().parse("((22)").is_err());
+}
+
+
+#[test]
+fn variable_declarationr() {
+    assert!(grammar::TermParser::new().parse("(22)").is_ok());
 }
