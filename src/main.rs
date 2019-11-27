f4d1913cd3d92cf764466dbe4b524b3194991aa7
Yohaï-Eliel Berreby <yberreby@protonmail.ch>
2019-11-27 11:30:42 +0100

    add lalrpop w/ example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub grammar); // synthesized by LALRPOP
+
+#[test]
+fn calculator1() {
+    assert!(grammar::TermParser::new().parse("22").is_ok());
+    assert!(grammar::TermParser::new().parse("(22)").is_ok());
+    assert!(grammar::TermParser::new().parse("((((22))))").is_ok());
+    assert!(grammar::TermParser::new().parse("((22)").is_err());
 }
