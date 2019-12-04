1828794f841efcc79853078cb3112aafa7dfbee9
Yohaï-Eliel Berreby <yberreby@protonmail.ch>
2019-12-04 11:26:12 +0100

    improve tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,22 @@ lalrpop_mod!(pub grammar); // synthesized by LALRPOP
 #[test]
 fn trivial_fn_decl() {
     grammar::FuncParser::new().parse("fn hello() {}").unwrap();
+}
+
+
+#[test]
+fn basic_fn_decl() {
     grammar::FuncParser::new().parse("fn hello(a: i64, b: f32) { (); (); }").unwrap();
 }
 
 
-
 #[test]
-fn statement_list() {
+fn statement_list_empty_stmt() {
     grammar::StatementListParser::new().parse("(); ();").unwrap();
 }
 
 
+#[test]
+fn statement_list_simple_assignment() {
+    grammar::StatementListParser::new().parse("a = 5;").unwrap();
+}
